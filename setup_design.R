8a37977f29ece1153208931abbaf337893bd27aa
Yvonne DeWit <50246552+ycdewit@users.noreply.github.com>
2019-11-07 18:45:58 -0500

    Delete setup_design.R

--- setup_design.R
+++
@@ -1,9 +0,0 @@
-setup_design <-
-  function(in_data, in_weights= "FWGT", in_repweights= "BSW[0-9]+", in_type="bootstrap", in_combined=TRUE) {
-    survey::svrepdesign(
-      data = in_data,
-      weights = as.formula(paste0("~",in_weights)),
-      repweights = in_repweights,
-      type = in_type,
-      combined.weights = in_combined)
-  }
